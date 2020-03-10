d381728b92f5fd974591db2ad4d6136824aa0064
Nathan Horton <vision.taking.form@gmail.com>
2020-03-10 18:09:44 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">
+        NavBar
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse" />
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">
+            Link
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <p>Icons</p>
+    <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'twitter' }" style="font-size: 44px;" />
+    <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }" style="font-size: 44px;" />
+    <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'instagram' }" style="font-size: 44px;" />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { NavbarPlugin } from 'bootstrap-vue'
+
+Vue.use(NavbarPlugin)
+
+export default {
+}
+</script>
+
+<style>
+</style>
